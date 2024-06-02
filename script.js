bd1a1200016bfbbdc1a1ba79e3d63df029325803
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-03 02:18:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', (event) => {
+   let arr = Array.from(document.querySelectorAll('.code'));
+  
+    arr.forEach((item, index) => {
+      item.addEventListener('input', () => {
+        if (item.value.length > 0) {
+          if (index < arr.length - 1) {
+            arr[index + 1].focus();
+          }
+        }
+      });
+  
+      item.addEventListener('keydown', (event) => {
+        if (event.key === 'Backspace' && item.value.length === 0) {
+          if (index > 0) {
+            arr[index - 1].focus();
+          }
+        }
+      });
+    });
+  
+    // Set the focus on the first input field when the page loads
+    if (arr.length > 0) {
+      arr[0].focus();
+    }
+  });
